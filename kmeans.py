3d1fffd41b9ac7d54fa4983ffec790055e6ac9d8
xiaoshuoyao <x5yao@ucsd.edu>
2021-06-05 17:03:21 +0800

    Create kmeans.py

---
+++ kmeans.py
@@ -0,0 +1,98 @@
+from collections import defaultdict
+from random import uniform
+from math import sqrt
+
+
+
+def point_avg(points):
+
+    dimensions = len(points[0])
+
+    new_center = []
+
+    for dimension in range(dimensions):
+        dim_sum = 0  # dimension sum
+        for p in points:
+            dim_sum += p[dimension]
+
+        # average of each dimension
+        new_center.append(dim_sum / float(len(points)))
+
+    return new_center
+def update_centers(data_set, assignments):
+    
+    new_means = defaultdict(list)
+    centers = []
+    for assignment, point in zip(assignments, data_set):
+        new_means[assignment].append(point)
+        
+    for points in new_means.values():
+        centers.append(point_avg(points))
+
+    return centers
+def assign_points(data_points, centers):
+    
+    assignments = []
+    for point in data_points:
+        shortest = float('inf')  # positive infinity
+        shortest_index = 0
+        for i in range(len(centers)):
+            val = distance(point, centers[i])
+            if val < shortest:
+                shortest = val
+                shortest_index = i
+        assignments.append(shortest_index)
+    return assignments
+
+
+def distance(a, b):
+    dimensions = len(a)
+    
+    _sum = 0
+    for dimension in range(dimensions):
+        difference_sq = (a[dimension] - b[dimension]) ** 2
+        _sum += difference_sq
+    return sqrt(_sum)
+
+
+def generate_k(data_set, k):
+    
+    centers = []
+    dimensions = len(data_set[0])
+    min_max = defaultdict(int)
+
+    for point in data_set:
+        
+        for i in range(dimensions):
+            val = point[i]
+            min_key = 'min_%d' % i
+            max_key = 'max_%d' % i
+            if min_key not in min_max or val < min_max[min_key]:
+                min_max[min_key] = val
+            if max_key not in min_max or val > min_max[max_key]:
+                min_max[max_key] = val
+
+    for _k in range(k):
+        rand_point = []
+        for i in range(dimensions):
+            min_val = min_max['min_%d' % i]
+            max_val = min_max['max_%d' % i]
+            
+            rand_point.append(uniform(min_val, max_val))
+
+        centers.append(rand_point)
+
+    return centers
+
+
+def k_means(dataset, k):
+    k_points = generate_k(dataset, k)
+    assignments = assign_points(dataset, k_points)
+    old_assignments = None
+    centers = None
+    while assignments != old_assignments:
+        new_centers = update_centers(dataset, assignments)
+        old_assignments = assignments
+        assignments = assign_points(dataset, new_centers)
+        centers = new_centers
+    return assignments, centers
